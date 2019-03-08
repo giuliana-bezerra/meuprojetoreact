31596920afde673556316a3957af2387f12eb143
Giuliana Silva Bezerra - Dataprev <giuliana.bezerra@dataprev.gov.br>
2019-03-08 15:53:56 -0300

    Navegação entre telas com React

---
+++ src/pages/home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home = () => (
+    <div className="title">
+        <h4>Home</h4>
+        <p>Essa é minha página inicial.</p>
+    </div>
+);
+
+export default Home;
