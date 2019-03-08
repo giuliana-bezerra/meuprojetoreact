ce098244955ce8c3207515b8aca1842923218718
Giuliana Silva Bezerra - Dataprev <giuliana.bezerra@dataprev.gov.br>
2019-03-08 10:07:29 -0300

    Construindo uma tela estática com React.

---
+++ src/components/footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+    <footer>
+        <p>&copy; 2019</p>
+    </footer>
+);
+
+export default Footer;
---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+    <header>
+        <h1>Meu Projeto React</h1>
+    </header>
+);
+
+export default Header;
